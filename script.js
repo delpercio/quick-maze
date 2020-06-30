61b430610f5da6b227747ed89ccc17dd5d481b9b
Steven Delpercio <stevendelpercio1992@hotmail.com>
2020-06-30 18:53:35 -0400

    final

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ let map = [
     "S     W W W W W W WWW",
     "WWWWW W W W W W W W W",
     "W     W W W   W W W W",
-    "W WWWWWWW WWWWW W W W",    
+    "W WWWWWWW WWWWW W W W",
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-let y=9;
-let x=0;
+let y = 9;
+let x = 0;
+let endX = 0;
+let endY = 0;
 
 let player = document.getElementById('player');
 const mazeEl = document.getElementById('maze');
@@ -27,23 +29,24 @@ document.addEventListener('keydown', movePlayer);
 const createMaze = function (blueprint) {
     for (let rowNum = 0; rowNum < blueprint.length; rowNum++) {
         const rowString = blueprint[rowNum]
-        let blockDivs ='';
+        let blockDivs = '';
         for (let colNum = 0; colNum < rowString.length; colNum++) {
             const blockType = rowString[colNum]
             if (blockType === 'W') {
                 blockDivs += '<div class="block wall"></div>'
 
-            
+
             } else if (blockType === 'S') {
                 blockDivs += '<div class="block start"></div>'
+
                 y = rowNum
                 x = colNum
 
             }
             else if (blockType === 'F') {
                 blockDivs += '<div class="block end"></div>'
-                y = rowNum
-                x = colNum
+                endY = rowNum
+                endX = colNum
             }
             else {
                 blockDivs += '<div class = "block"></div>'
@@ -60,36 +63,49 @@ createMaze(map)
 function movePlayer(event) {
     let player = document.getElementById('player');
 
-    if (event.code === 'ArrowUp' ) {
-        y = y - 1  
-        console.log(x,y)
-        console.log(map[x][y])
-        
+    if (event.code === 'ArrowUp' && map[y - 1][x] !== "W" && map[y-1][x] === " " || map[y-1][x] === "S" || map[y-1][x] === "F") {
+        y = y - 1
+        console.log(y, x)
+        console.log(map[y][x])
+        checkWinner(event)
+
     }
-    else if (event.code === 'ArrowDown') {
+    else if (event.code === 'ArrowDown' && map[y + 1][x] !== "W" && map[y+1][x] === " " || map[y+1][x] === "S" || map[y+1][x] === "F") {
         y = y + 1
-        console.log(x,y)
-        console.log(map[x][y])
+        console.log(y, x)
+        console.log(map[y][x])
+        checkWinner(event)
+
     }
-    else if (event.code === 'ArrowLeft') {
+    else if (event.code === 'ArrowLeft' && map[y][x - 1] !== "W" && map[y][x - 1] === " " || map[y][x - 1] === "S" || map[y][x - 1] === "F")  {
         x = x - 1
-        console.log(x,y)
-        console.log(map[x][y])
+        console.log(y, x)
+        console.log(map[y][x])
+        checkWinner(event)
+
     }
-    else if (event.code === 'ArrowRight' ) {
+    else if (event.code === 'ArrowRight' && map[y][x + 1] !== "W" && map[y][x+1] === " " || map[y][x+1] === "S" || map[y][x+1] === "F") {
         x = x + 1
-        console.log(x,y)
-        console.log(map[x][y])
+        console.log(y, x)
+        console.log(map[y][x])
+        checkWinner(event)
+
     }
 
-    
+
     player.style.top = y * 20 + "px";
     player.style.left = x * 20 + "px";
-    
+
 }
 
 
 
 
-
+function checkWinner(event) {
+    if (x+y === endX+endY){
+        let winnerDiv = document.getElementById('winner')
+        winnerDiv.innerHTML = 'YOU ARE A WINNER!!'
+    }
+    
+}
 
